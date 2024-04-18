111d339567b79d6c80ff269946f273a4bd933f68
Ko-Aung2100 <aungkaunghtetps2100@gmail.com>
2024-04-18 18:29:15 +0700

    modify to classify students into A,B,C and failed according to gpa. Produce different text files

--- Main1.java
+++ Main1.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main1 {
@@ -14,45 +15,92 @@ public class Main1 {
                 line = reader.readLine();
             }
             reader.close();
-            String id = ""; String name = " "; double gpa = 0;
+            String id = "";
+            String name = " ";
+            double gpa = 0;
             reader1 = new BufferedReader(new FileReader("out.txt"));
             Student[] students = new Student[count];
             int i = 0;
             String line1 = reader1.readLine(); // Move this line outside the loop
             while (line1 != null) {
-              
-               String[] arr = line1.split(" ");
-               id = arr[0];
-               name = arr[1];
-               gpa = Double.parseDouble(arr[2]);
-                students[i] = new Student(id,name,gpa);
+
+                String[] arr = line1.split(" ");
+                id = arr[0];
+                name = arr[1];
+                gpa = Double.parseDouble(arr[2]);
+                students[i] = new Student(id, name, gpa);
                 i++;
                 line1 = reader1.readLine();
             }
             double AGpa = 3.50;
             double BGpa = 2.50;
             double CGpa = 2.00;
-           for (Student student : students) {
-               if( student.getGpa() >= AGpa) {
-                    System.out.println("class A students : " + student.getFullName());
-               }
-               if(AGpa > student.getGpa() && student.getGpa() >= BGpa){
-                    System.out.println("class B students : " + student.getFullName());
-               }
-               if(BGpa > student.getGpa() && student.getGpa() >= CGpa){
-                System.out.println("class C students : " + student.getFullName());
-               }
-               if(student.getGpa() < CGpa){
-                 System.out.println("failed students : " + student.getFullName());
-               }
-           }
+            int countA = 0;
+            int countB = 0;
+            int countC = 0;
+            int countf = 0;
+            for (Student student : students) {
+                if (student.getGpa() >= AGpa) {
+                    countA++;
+                }
+                if (AGpa > student.getGpa() && student.getGpa() >= BGpa) {
+                    countB++;
+                }
+                if (BGpa > student.getGpa() && student.getGpa() >= CGpa) {
+                    countC++;
+                }
+                if (student.getGpa() < CGpa) {
+                    countf++;
+                }
+            }
             reader1.close();
-          
+            Student[] studentsA = new Student[countA];
+            Student[] studentsB = new Student[countB];
+            Student[] studentsC = new Student[countC];
+            Student[] failedStudents = new Student[countf];
+            int a = 0; int b = 0; int c = 0; int f = 0;
+            for (Student student : students) {
+                if (student.getGpa() >= AGpa) {
+                    studentsA[a] = new Student(student.getId(),student.getFullName(),student.getGpa());
+                    a++;
+                }
+                if (AGpa > student.getGpa() && student.getGpa() >= BGpa) {
+                    studentsB[b] = new Student(student.getId(),student.getFullName(),student.getGpa());
+                    b++;
+                }
+                if (BGpa > student.getGpa() && student.getGpa() >= CGpa) {
+                    studentsC[c] = new Student(student.getId(),student.getFullName(),student.getGpa());
+                    c++;
+                }
+                if (student.getGpa() < CGpa) {
+                    failedStudents[f] = new Student(student.getId(),student.getFullName(),student.getGpa());
+                    f++;
+                }
+            }
+            writeToFile(studentsA, "classA.txt");
+            writeToFile(studentsB, "classB.txt");
+            writeToFile(studentsC, "classC.txt");
+            writeToFile(failedStudents, "faileStudents.txt");
 
         } catch (IOException e) {
             System.out.println("error");
             e.printStackTrace();
         }
 
+    }
+    public static void writeToFile(Student[] students,String fileName){
+        try {
+            FileWriter writer= new FileWriter(fileName, true);
+            for(int i = 0; i < students.length ; i++){
+                writer.write(students[i].toString());
+                writer.write("\n");
+            }
+            writer.close();
+            System.out.println("sucessfully wrote");
+        } catch (IOException e) {
+            // TODO: handle exception
+            System.out.println("error");
+            e.printStackTrace();
+        }
     }
 }
